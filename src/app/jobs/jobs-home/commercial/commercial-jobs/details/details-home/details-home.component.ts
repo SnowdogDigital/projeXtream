8f1708b6530a6875ab864556390666aa1f92ba96
craig <craig@cerberus-cyphers.com>
2023-08-27 20:59:47 -0400

    prev & next buttons working, cleaned up some more commented out lines

--- src/app/jobs/jobs-home/commercial/commercial-jobs/details/details-home/details-home.component.ts
+++ src/app/jobs/jobs-home/commercial/commercial-jobs/details/details-home/details-home.component.ts
@@ -9,7 +9,7 @@ import { StatusesService } from '../../../../../../services/statuses.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { selectJobById,selectStatusById } from 'src/app/store/jobs-table-selectors';
+import { getSelectJob, jobSelector, selectJobById,selectJobByIndex,selectStatusById } from 'src/app/store/jobs-table-selectors';
 import { changeSelectedJobIndex } from 'src/app/store/jobs-table-actions';
 
 @Component({
@@ -25,37 +25,48 @@ export class DetailsHomeComponent implements OnInit {
   faMap = faMap;
   faSquareXmark = faSquareXmark;
 
-  // jobs$: Observable<Job[]>; 
-  currentJob$: Observable<Job>;
+  currentJob$: Observable<Job | null>;
   statuses$: Observable<Status>;
   hidden = true;
   status: number;
   selectedJobIndex: number;
   
-  constructor(private store: Store<{jobs: Job[]}>, private jobsService: JobsService, private statusesService: StatusesService, private route: ActivatedRoute, private router: Router) {
+  constructor(private store: Store<{}>, private jobsService: JobsService, private statusesService: StatusesService, private route: ActivatedRoute, private router: Router) {
   }
   
   ngOnInit(): void {
-    // this.statuses = this.statusesService.getStatuses();
-    // this.currentJob = this.jobsService.getJob(this.selectedJobId);
     const getId = this.route.snapshot.paramMap.get('id');
     const jobId = Number.parseInt(getId!);
     let statusId = 0;
     this.currentJob$ = this.store.select(selectJobById(jobId));
     this.statuses$ = this.store.select(selectStatusById(statusId));
     //! status.name from job.status isnt working yet ^
-    // console.log(this.currentJob$.order_no);
-
-  }
-
-
-  onClickPrev() {
-   this.store.dispatch(changeSelectedJobIndex({delta: -1}));
-  }
-  onClickNext() {
-    this.store.dispatch(changeSelectedJobIndex({delta: 1}));
+    
   }
   
+  
+  onClickPrev() {
+    this.store.dispatch(changeSelectedJobIndex({ delta: -1 }));
+  
+    this.store.select(getSelectJob).subscribe(jobState => {
+      const selectedJobIndex = jobState.selectedJobIndex;
+      if (selectedJobIndex !== null) {
+        this.currentJob$ = this.store.select(selectJobByIndex(selectedJobIndex));
+      }
+    });
+  }
+  
+  onClickNext() {
+    this.store.dispatch(changeSelectedJobIndex({ delta: 1 }));
+  
+    this.store.select(getSelectJob).subscribe(jobState => {
+      const selectedJobIndex = jobState.selectedJobIndex;
+      if (selectedJobIndex !== null) {
+        this.currentJob$ = this.store.select(selectJobByIndex(selectedJobIndex));
+      }
+    });
+  }
+    
 
   toggleNotes(e: any) {
     console.log(e.target);
--- src/app/store/jobs-table-selectors.ts
+++ src/app/store/jobs-table-selectors.ts
@@ -22,5 +22,14 @@ export const statusesSelector = createSelector(getSelectStatus, (state: JobState
 
 export const selectJobById = (jobId: number) => createSelector(getSelectJob, (state: JobState)=>state.jobs.filter(j => j.id === jobId)[0]);
 
+export const selectJobByIndex = (index: number) =>
+  createSelector(getSelectJob, (state: JobState) => {
+    if (index >= 0 && index < state.jobs.length) {
+      return state.jobs[index];
+    } else {
+      return null; // Handle out-of-bounds index
+    }
+  });
+
 export const selectStatusById = (statusId: number) => createSelector(getSelectStatus, (state: JobState)=>state.statuses.filter(s => s.id === statusId)[0]);
 //! status.name from job.status isnt working yet ^
